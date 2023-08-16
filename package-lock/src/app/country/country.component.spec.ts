de88c03d7f88baf6b570ed9051fd242aa33845a7
nileshpatil0529 <56604622+nileshpatil0529@users.noreply.github.com>
2023-08-17 00:56:50 +0530

    Unit test's added

---
+++ package-lock/src/app/country/country.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CountryComponent } from './country.component';
+import { CountryService } from '../service/country.service';
+import { of } from 'rxjs';
+
+describe('CountryComponent', () => {
+  let component: CountryComponent;
+  let fixture: ComponentFixture<CountryComponent>;
+  let mockCountryService: jasmine.SpyObj<CountryService>;
+
+  beforeEach(() => {
+    const countryServiceSpy = jasmine.createSpyObj('CountryService', ['getOneCountry']);
+
+    TestBed.configureTestingModule({
+      declarations: [CountryComponent],
+      providers: [{ provide: CountryService, useValue: countryServiceSpy }],
+    });
+
+    fixture = TestBed.createComponent(CountryComponent);
+    component = fixture.componentInstance;
+    mockCountryService = TestBed.inject(CountryService) as jasmine.SpyObj<CountryService>;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set countryData when ngOnInit is called', () => {
+    const mockCountryData = {};
+    mockCountryService.getOneCountry.and.returnValue(of(mockCountryData));
+
+    component.ngOnInit();
+
+    expect(component.countryData).toEqual(mockCountryData);
+  });
+});
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CountryComponent } from './country/country.component';
+import { CountryService } from './service/country.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RegionComponent } from './region/region.component';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, CountryComponent, RegionComponent],
+      imports: [FormsModule, HttpClientModule, StoreModule.forRoot({})],
+      providers: [
+        {
+          provide: CountryService,
+          useValue: {},
+        },
+      ],
+    }).compileComponents();
+  
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+  
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+});
