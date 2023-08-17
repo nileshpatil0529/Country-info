834e3a1d750e6b0970c883d80088527bfbe71b14
nileshpatil0529 <56604622+nileshpatil0529@users.noreply.github.com>
2023-08-17 17:22:33 +0530

    Delete src/app/store directory

--- src/app/store/regions.selector.ts
+++
@@ -1,8 +0,0 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-
-
-const getRegionState = createFeatureSelector<any>('region');
-
-export const getRegions = createSelector(getRegionState, (state) => {
-    return state;
-});
